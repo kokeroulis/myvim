39659bac21b85eba8bc6010fc4e30209fc15bfca
Giorgos Tsiapaliokas <terietor@gmail.com>
2013-08-27 16:05:36 +0300

    make it more complete

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,7 @@
 function downloadOrUpdate()
 {
-    githubRepo = $1
-    repositoryName = $2
+    githubRepo=$1
+    repositoryName=$2
     if [ ! -d $i ]
      then
         git clone $githubRepo/$repositoryName
@@ -9,10 +9,16 @@ function downloadOrUpdate()
         cd $i
         git pull
         cd ..
+        sleep 1
     fi
 }
 
+function info()
+{
+    echo "===============> "$1
+}
 
+info "checking for ~/.vimrc"
 
 if [ ! -L ~/.vimrc ]
 then
@@ -24,6 +30,11 @@ user1Repositories[1]="vim-handlebars"
 user1Repositories[2]="vim-qml"
 user1Repositories[3]="jade.vim"
 user1Repositories[4]="AutoComplPop"
+
+user2Repositories[0]="vim-kokeroulis"
+
+
+info "checking for pathogen"
 
 if [ ! -d ~/.vim/autoload ] || [ ! -d ~/.vim/bundle ]
 then
@@ -37,11 +48,16 @@ then
         https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 fi
 
+info "downloading/updating repos"
 cd ~/.vim/bundle
 
 for i in ${user1Repositories[@]}
 do
-    downloadOrUpdate "https://githib.com/terietor" $i
+    downloadOrUpdate "git://github.com/terietor" $i
 done
 
+for i in ${user2Repositories[@]}
+do
+    downloadOrUpdate "git://github.com/kokeroulis" $i
+done
 
